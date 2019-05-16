c5884926a995fc69af8697279ba3adfd20fd1f5f
Dale Stammen <dalestam@microsoft.com>
2019-05-16 12:06:46 -0700

    initial commit

---
+++ WindowsRuntimeComponent1/Class1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace WindowsRuntimeComponent1;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::System;
+
+using namespace Windows::UI::Core;
+
+Class1::Class1()
+{
+    auto window = CoreWindow::GetForCurrentThread();
+    window->PointerMoved += ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &Class1::OnPointerMoved);
+}
+
+
+
+void Class1::OnPointerMoved(CoreWindow^ window, PointerEventArgs^ args)
+{
+    OnPointerMovedEvent(args->CurrentPoint->Position.X, args->CurrentPoint->Position.Y);
+}
+
---
+++ WindowsRuntimeComponent1/Class1.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+namespace WindowsRuntimeComponent1
+{
+    public delegate void OnPointerMovedHandler(float x, float y);
+
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+
+        event OnPointerMovedHandler^ OnPointerMovedEvent;
+
+    private:
+        void OnPointerMoved(Windows::UI::Core::CoreWindow^ window, Windows::UI::Core::PointerEventArgs^ args);
+    };
+}
